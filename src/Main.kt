e7e68857c06e7b13fa9fb04777b7cf32f3a0563f
Portmanteaux26 <michael.a.ba@gmail.com>
2024-12-11 16:20:25 -0600

    Implement Main.day01b

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,18 @@
-import java.io.File;
+import java.io.File
 import kotlin.math.abs
 
 
 fun day01a() : Int {
 
-    val (leftCol, rightCol, numIDs) = day01Input()
+    val (leftList, rightList, numIDs) = day01Input()
 
-    leftCol.sort()
-    rightCol.sort()
+    leftList.sort()
+    rightList.sort()
 
     var totalDist = 0
     for (i in 0..<numIDs) {
-        val leftID = leftCol[i].toInt()
-        val rightID = rightCol[i].toInt()
+        val leftID = leftList[i].toInt()
+        val rightID = rightList[i].toInt()
         val dist = abs(leftID - rightID)
         totalDist += dist
     }
@@ -20,24 +20,48 @@ fun day01a() : Int {
     return totalDist
 }
 
+fun day01b() : Int {
+
+    val (leftList, rightList, numIDs) = day01Input()
+
+    val countMap = mutableMapOf<String, Int>()
+    for (i in 0..<numIDs) {
+        val leftID = leftList[i]
+        for (rightID in rightList) {
+            if (rightID == leftID) {
+                countMap[leftID] = countMap.getOrDefault(rightID, 0) + 1
+            }
+        }
+    }
+
+    var simScore = 0
+    for (id in countMap) {
+        simScore += id.key.toInt() * id.value
+    }
+
+    return simScore
+}
+
 private fun day01Input(): Triple<MutableList<String>, MutableList<String>, Int> {
 
     val rawInput = File("input01.txt").readLines()
 
-    val leftCol = mutableListOf<String>()
-    val rightCol = mutableListOf<String>()
+    val leftList = mutableListOf<String>()
+    val rightList = mutableListOf<String>()
     for (line in rawInput) {
         val pair = line.split("   ")
-        leftCol.add(pair[0])
-        rightCol.add(pair[1])
+        leftList.add(pair[0])
+        rightList.add(pair[1])
     }
-    val numIDs = leftCol.size
-    assert(numIDs == rightCol.size && numIDs == rawInput.size)
+    val numIDs = leftList.size
+    assert(numIDs == rightList.size && numIDs == rawInput.size)
 
-    return Triple(leftCol, rightCol, numIDs)
+    return Triple(leftList, rightList, numIDs)
 }
 
 fun main() {
     val totalDist = day01a()
     println(totalDist)
+    val simScore =day01b()
+    println(simScore)
 }
