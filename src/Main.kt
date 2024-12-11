a19402c3813225bfe4946384b4b943342ad3ff80
Portmanteaux26 <michael.a.ba@gmail.com>
2024-12-11 14:07:07 -0600

    Refactor: Rename Main.day01 to Main.day01a Extract Main.day01input

--- src/Main.kt
+++ src/Main.kt
@@ -2,24 +2,9 @@ import java.io.File;
 import kotlin.math.abs
 
 
-fun main() {
-    val totalDist = day01()
-    println(totalDist)
-}
+fun day01a() : Int {
 
-fun day01() : Int {
-
-    val rawInput = File("input01.txt").readLines()
-
-    val leftCol = mutableListOf<String>()
-    val rightCol = mutableListOf<String>()
-    for (line in rawInput) {
-        val pair = line.split("   ")
-        leftCol.add(pair[0])
-        rightCol.add(pair[1])
-    }
-    val numIDs = leftCol.size
-    assert(numIDs == rightCol.size && numIDs == rawInput.size)
+    val (leftCol, rightCol, numIDs) = day01Input()
 
     leftCol.sort()
     rightCol.sort()
@@ -33,4 +18,26 @@ fun day01() : Int {
     }
 
     return totalDist
+}
+
+private fun day01Input(): Triple<MutableList<String>, MutableList<String>, Int> {
+
+    val rawInput = File("input01.txt").readLines()
+
+    val leftCol = mutableListOf<String>()
+    val rightCol = mutableListOf<String>()
+    for (line in rawInput) {
+        val pair = line.split("   ")
+        leftCol.add(pair[0])
+        rightCol.add(pair[1])
+    }
+    val numIDs = leftCol.size
+    assert(numIDs == rightCol.size && numIDs == rawInput.size)
+
+    return Triple(leftCol, rightCol, numIDs)
+}
+
+fun main() {
+    val totalDist = day01a()
+    println(totalDist)
 }
