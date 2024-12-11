6d99a33c4af943905efe606e6b7ab6117d31da88
Portmanteaux26 <michael.a.ba@gmail.com>
2024-12-11 16:42:06 -0600

    Extract day01.kt

---
+++ src/day01.kt
@@ -0,0 +1,60 @@
+import java.io.File
+import kotlin.math.abs
+
+
+fun totalDistance() : Int {
+
+    val (leftList, rightList, numIDs) = day01Input()
+
+    leftList.sort()
+    rightList.sort()
+
+    var totalDist = 0
+    for (i in 0..<numIDs) {
+        val leftID = leftList[i].toInt()
+        val rightID = rightList[i].toInt()
+        val dist = abs(leftID - rightID)
+        totalDist += dist
+    }
+
+    return totalDist
+}
+
+fun similarityScore() : Int {
+
+    val (leftList, rightList, numIDs) = day01Input()
+
+    val countMap = mutableMapOf<String, Int>()
+    for (i in 0..<numIDs) {
+        val leftID = leftList[i]
+        for (rightID in rightList) {
+            if (rightID == leftID) {
+                countMap[leftID] = countMap.getOrDefault(rightID, 0) + 1
+            }
+        }
+    }
+
+    var simScore = 0
+    for (id in countMap) {
+        simScore += id.key.toInt() * id.value
+    }
+
+    return simScore
+}
+
+private fun day01Input(): Triple<MutableList<String>, MutableList<String>, Int> {
+
+    val rawInput = File("input01.txt").readLines()
+
+    val leftList = mutableListOf<String>()
+    val rightList = mutableListOf<String>()
+    for (line in rawInput) {
+        val pair = line.split("   ")
+        leftList.add(pair[0])
+        rightList.add(pair[1])
+    }
+    val numIDs = leftList.size
+    assert(numIDs == rightList.size && numIDs == rawInput.size)
+
+    return Triple(leftList, rightList, numIDs)
+}
