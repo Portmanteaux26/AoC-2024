1a8fbae6cebaa33be39817a82cf5097115c01ef5
Portmanteaux26 <michael.a.ba@gmail.com>
2024-12-16 16:59:21 -0600

    Save work on filter

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,4 @@
 fun main() {
     val input = day03Input("input03.txt")
-    println(input)
+    println(filter(input))
 }
--- src/day03.kt
+++ src/day03.kt
@@ -5,3 +5,12 @@ fun day03Input(path: String): String {
     val rawInput = File(path).readText()
     return rawInput
 }
+
+fun filter(rawInput: String): String {
+
+    val reg = Regex("mul(.+,.+)")
+    val matches = reg.findAll(rawInput)
+    val instructions = matches.map{it.groupValues}.joinToString()
+
+    return instructions
+}
