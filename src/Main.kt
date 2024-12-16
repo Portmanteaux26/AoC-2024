837be41f82cafae06b85052ba42a530875f88b62
Portmanteaux26 <michael.a.ba@gmail.com>
2024-12-16 16:31:34 -0600

    Implement day03Input

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,4 @@
 fun main() {
-    val reports = day02input()
-    println(safeReports(reports))
+    val input = day03Input("input03.txt")
+    println(input)
 }
---
+++ src/day03.kt
@@ -0,0 +1,7 @@
+import java.io.File
+
+
+fun day03Input(path: String): String {
+    val rawInput = File(path).readText()
+    return rawInput
+}
